0d1c7f8f83c5d5f03c5c57055359107ff112ecd7
ApacheTomdog <122610466+apachetomdogs@users.noreply.github.com>
2024-08-29 15:14:20 +0900

    [level 0] Title: n의 배수, Time: 0.03 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181937. n의 배수/n의 배수.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int num, int n) {
+        int answer = 0;
+        if(num >=2 && num <= 100 && n >=2 && n <=9){
+        if(num % n == 0){
+            answer = 1;
+        }
+        else {
+            answer = 0;
+        }
+        }
+        return answer;
+    }
+}
